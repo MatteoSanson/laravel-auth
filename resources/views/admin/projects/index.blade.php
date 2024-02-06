6c0df0e8755a4d52e7f97174aebd771dcabf24b8
MatteoSanson <matteo.sanson@gmail.com>
2024-02-06 20:41:29 +0100

    feat: view table projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.admin')
 
 @section('content')
-    <ul>
+    {{-- <ul>
         @foreach ($projects as $project)
             <li>{{ $project->title }}</li>
         @endforeach
-    </ul>
+    </ul> --}}
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Type</th>
+                <th scope="col">Visibility</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->type }}</td>
+                    <td>{{ $project->visibility }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
